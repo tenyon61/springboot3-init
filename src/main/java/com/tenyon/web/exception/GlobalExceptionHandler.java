9891e80b1cdf3ca7330844c49d52b4bb6cec16f0
tenyon61 <tenyon@cqbo.com>
2025-04-02 16:00:47 +0800

    fix: 添加校验失败的全局异常处理器

--- src/main/java/com/tenyon/web/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tenyon/web/exception/GlobalExceptionHandler.java
@@ -4,8 +4,12 @@ import com.tenyon.web.common.BaseResponse;
 import com.tenyon.web.common.ResultUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 /**
  * 全局异常处理器
@@ -24,5 +28,14 @@ public class GlobalExceptionHandler {
     public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
         logger.error("RuntimeException", e);
         return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public BaseResponse<?> handleException(Exception ex) {
+        if (ex instanceof BindException) {
+            String errorMsg = ((BindException) ex).getBindingResult().getFieldErrors().stream().map(FieldError::getDefaultMessage).sorted().collect(Collectors.joining(","));
+            return ResultUtils.error(ErrorCode.SYSTEM_ERROR, errorMsg);
+        }
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR);
     }
 }
